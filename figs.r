eab4ee0c0f7df956c1a4ad3187248a80e5c26664
antonatosc <156329470+antonatosc@users.noreply.github.com>
2024-02-29 22:32:06 +0200

    Update figs.r

--- figs.r
+++ figs.r
@@ -1,8 +1,10 @@
 load("D:/data/skin_neuroticism/clusters/scripts/res.RData")
 get_labels_and_metrics = function(df){
   df$OR = exp(df$Beta)
-  df$LO_CI = exp(df$Beta - 1.96*df$SE)
-  df$UP_CI = exp(df$Beta + 1.96*df$SE)
+  df$loci = df$Beta - 1.96*df$SE
+  df$upci = df$Beta + 1.96*df$SE
+  df$LO_CI = exp(df$loci)
+  df$UP_CI = exp(df$upci)
   df$label = paste0(round(df$OR, 3), " (", round(df$LO_CI, 3), "-", round(df$UP_CI, 3), ")")
   return(df)
 }
@@ -21,7 +23,7 @@ for(i in names(diseases_as_exposure[["psoriasis"]])){
   psoriasis_exposure_res[["mregger"]] = rbind(psoriasis_exposure_res[["mregger"]], data.frame(
     Exposure = diseases_as_exposure[["psoriasis"]][[i]]$results$id.exposure[1],
     Outcome = diseases_as_exposure[["psoriasis"]][[i]]$results$id.outcome[1],
-    Method = "MR Egger",
+    Method = "MR-Egger",
     NSNPs = diseases_as_exposure[["psoriasis"]][[i]]$results$nsnp[1],
     Beta = diseases_as_exposure[["psoriasis"]][[i]]$results$b[1],
     SE = diseases_as_exposure[["psoriasis"]][[i]]$results$se[1],
@@ -63,7 +65,7 @@ psoriasis_exposure_res$Outcome = factor(psoriasis_exposure_res$Outcome, levels =
 library(ggplot2)
 library(dplyr)
 psoriasis_exp_plot = psoriasis_exposure_res$Method <- factor(psoriasis_exposure_res$Method, 
-                                                             levels = c("MR-PRESSO", "Weighted median", "MR Egger", "IVW"))
+                                                             levels = c("MR-PRESSO", "Weighted median", "MR-Egger", "IVW"))
 psoriasis_exposure_plot = ggplot(psoriasis_exposure_res,
                                  aes(x = OR,
                                      y = reorder(Outcome, desc(Outcome)),
@@ -78,10 +80,10 @@ psoriasis_exposure_plot = ggplot(psoriasis_exposure_res,
              linetype = "dotted",
              color = "black") +
   scale_color_manual(values = c("IVW" = "#990438",
-                                "MR Egger" = "#FFC107",
+                                "MR-Egger" = "#FFC107",
                                 "Weighted median" = "#004D40",
                                 "MR-PRESSO" = "#1E88E5"),
-                     breaks = c("IVW", "Weighted median", "MR Egger", "MR-PRESSO")) +
+                     breaks = c("IVW", "Weighted median", "MR-Egger", "MR-PRESSO")) +
   theme_classic(base_size = 16) +
   labs(x = "Odds Ratio (95% Confidence Interval)",
        y = "",
@@ -91,7 +93,8 @@ psoriasis_exposure_plot = ggplot(psoriasis_exposure_res,
     axis.text.x = element_text(size = 12, color = "black"),
     axis.text.y = element_text(size = 12, color = "black"),
     axis.ticks = element_line(color = "black"),
-    strip.background = element_rect(colour = "white", fill = "white")
+    strip.background = element_rect(colour = "white", fill = "white"),
+    legend.position = "none"
   )
 psoriasis_outcome_res = list()
 for(i in names(diseases_as_outcome[["psoriasis"]])){
@@ -107,7 +110,7 @@ for(i in names(diseases_as_outcome[["psoriasis"]])){
   psoriasis_outcome_res[["simex"]] = rbind(psoriasis_outcome_res[["simex"]], data.frame(
     Exposure = diseases_as_outcome[["psoriasis"]][[i]]$results$id.exposure[1],
     Outcome = diseases_as_outcome[["psoriasis"]][[i]]$results$id.outcome[1],
-    Method = "MR Egger-SIMEX",
+    Method = "MR-Egger/SIMEX",
     NSNPs = diseases_as_outcome[["psoriasis"]][[i]]$results$nsnp[1],
     Beta = diseases_as_outcome[["psoriasis"]][[i]]$SIMEX$beta[1],
     SE = diseases_as_outcome[["psoriasis"]][[i]]$SIMEX$se[1],
@@ -146,10 +149,8 @@ psoriasis_outcome_res[psoriasis_outcome_res$Exposure == "worry_cluster", ]$Expos
 psoriasis_outcome_res[psoriasis_outcome_res$Exposure == "depressed_cluster", ]$Exposure = "Depressed affect"
 psoriasis_outcome_res[psoriasis_outcome_res$Exposure == "SESA", ]$Exposure = "SESA"
 psoriasis_outcome_res$Exposure = factor(psoriasis_outcome_res$Exposure, levels = c("Neuroticism", "Depressed affect", "SESA", "Worry"))
-library(ggplot2)
-library(dplyr)
 psoriasis_outc_plot = psoriasis_outcome_res$Method <- factor(psoriasis_outcome_res$Method, 
-                                                             levels = c("MR-PRESSO", "Weighted median", "MR Egger-SIMEX", "IVW"))
+                                                             levels = c("MR-PRESSO", "Weighted median", "MR-Egger/SIMEX", "IVW"))
 psoriasis_outcome_plot = ggplot(psoriasis_outcome_res,
                                 aes(x = OR,
                                     y = reorder(Exposure, desc(Exposure)),
@@ -164,10 +165,10 @@ psoriasis_outcome_plot = ggplot(psoriasis_outcome_res,
              linetype = "dotted",
              color = "black") +
   scale_color_manual(values = c("IVW" = "#990438",
-                                "MR Egger-SIMEX" = "#FFC107",
+                                "MR-Egger/SIMEX" = "#FFC107",
                                 "Weighted median" = "#004D40",
                                 "MR-PRESSO" = "#1E88E5"),
-                     breaks = c("IVW", "Weighted median", "MR Egger", "MR-PRESSO")) +
+                     breaks = c("IVW", "Weighted median", "MR-Egger/SIMEX", "MR-PRESSO")) +
   theme_classic(base_size = 16) +
   labs(x = "Odds Ratio (95% Confidence Interval)",
        y = "",
@@ -180,6 +181,12 @@ psoriasis_outcome_plot = ggplot(psoriasis_outcome_res,
     strip.background = element_rect(colour = "white", fill = "white")
   ) +
   coord_cartesian(xlim = c(0.5, 3))
+pdf("D:/data/skin_neuroticism/clusters/Fig1.pdf",
+width = 12,
+height = 5
+)
+cowplot::plot_grid(psoriasis_exposure_plot, psoriasis_outcome_plot, rel_widths = c(1, 1.2), scale = 1, labels = "auto")
+dev.off()
 ###eczema
 eczema_exposure_res = list()
 for(i in names(diseases_as_exposure[["eczema"]])){
@@ -195,7 +202,7 @@ for(i in names(diseases_as_exposure[["eczema"]])){
   eczema_exposure_res[["mregger"]] = rbind(eczema_exposure_res[["mregger"]], data.frame(
     Exposure = diseases_as_exposure[["eczema"]][[i]]$results$id.exposure[1],
     Outcome = diseases_as_exposure[["eczema"]][[i]]$results$id.outcome[1],
-    Method = "MR Egger",
+    Method = "MR-Egger",
     NSNPs = diseases_as_exposure[["eczema"]][[i]]$results$nsnp[1],
     Beta = diseases_as_exposure[["eczema"]][[i]]$results$b[1],
     SE = diseases_as_exposure[["eczema"]][[i]]$results$se[1],
@@ -228,7 +235,7 @@ for(i in names(diseases_as_exposure[["eczema"]])){
 }
 eczema_exposure_res = do.call(rbind, eczema_exposure_res)
 eczema_exposure_res = get_labels_and_metrics(eczema_exposure_res)
-eczema_exposure_res[eczema_exposure_res$Exposure == "eczema", ]$Exposure = "Atopic dermatitis"
+eczema_exposure_res[eczema_exposure_res$Exposure == "eczema", ]$Exposure = "eczema"
 eczema_exposure_res[eczema_exposure_res$Outcome == "neuro", ]$Outcome = "Neuroticism"
 eczema_exposure_res[eczema_exposure_res$Outcome == "worry_cluster", ]$Outcome = "Worry"
 eczema_exposure_res[eczema_exposure_res$Outcome == "depressed_cluster", ]$Outcome = "Depressed affect"
@@ -237,7 +244,7 @@ eczema_exposure_res$Outcome = factor(eczema_exposure_res$Outcome, levels = c("Ne
 library(ggplot2)
 library(dplyr)
 eczema_exp_plot = eczema_exposure_res$Method <- factor(eczema_exposure_res$Method, 
-                                                             levels = c("MR-PRESSO", "Weighted median", "MR Egger", "IVW"))
+                                                             levels = c("MR-PRESSO", "Weighted median", "MR-Egger", "IVW"))
 eczema_exposure_plot = ggplot(eczema_exposure_res,
                                  aes(x = OR,
                                      y = reorder(Outcome, desc(Outcome)),
@@ -252,10 +259,10 @@ eczema_exposure_plot = ggplot(eczema_exposure_res,
              linetype = "dotted",
              color = "black") +
   scale_color_manual(values = c("IVW" = "#990438",
-                                "MR Egger" = "#FFC107",
+                                "MR-Egger" = "#FFC107",
                                 "Weighted median" = "#004D40",
                                 "MR-PRESSO" = "#1E88E5"),
-                     breaks = c("IVW", "Weighted median", "MR Egger", "MR-PRESSO")) +
+                     breaks = c("IVW", "Weighted median", "MR-Egger", "MR-PRESSO")) +
   theme_classic(base_size = 16) +
   labs(x = "Odds Ratio (95% Confidence Interval)",
        y = "",
@@ -265,7 +272,8 @@ eczema_exposure_plot = ggplot(eczema_exposure_res,
     axis.text.x = element_text(size = 12, color = "black"),
     axis.text.y = element_text(size = 12, color = "black"),
     axis.ticks = element_line(color = "black"),
-    strip.background = element_rect(colour = "white", fill = "white")
+    strip.background = element_rect(colour = "white", fill = "white"),
+    legend.position = "none"
   )
 eczema_outcome_res = list()
 for(i in names(diseases_as_outcome[["eczema"]])){
@@ -281,7 +289,7 @@ for(i in names(diseases_as_outcome[["eczema"]])){
   eczema_outcome_res[["simex"]] = rbind(eczema_outcome_res[["simex"]], data.frame(
     Exposure = diseases_as_outcome[["eczema"]][[i]]$results$id.exposure[1],
     Outcome = diseases_as_outcome[["eczema"]][[i]]$results$id.outcome[1],
-    Method = "MR Egger-SIMEX",
+    Method = "MR-Egger/SIMEX",
     NSNPs = diseases_as_outcome[["eczema"]][[i]]$results$nsnp[1],
     Beta = diseases_as_outcome[["eczema"]][[i]]$SIMEX$beta[1],
     SE = diseases_as_outcome[["eczema"]][[i]]$SIMEX$se[1],
@@ -314,16 +322,14 @@ for(i in names(diseases_as_outcome[["eczema"]])){
 }
 eczema_outcome_res = do.call(rbind, eczema_outcome_res)
 eczema_outcome_res = get_labels_and_metrics(eczema_outcome_res)
-eczema_outcome_res[eczema_outcome_res$Outcome == "eczema", ]$Outcome = "Atopic dermatitis"
+eczema_outcome_res[eczema_outcome_res$Outcome == "eczema", ]$Outcome = "eczema"
 eczema_outcome_res[eczema_outcome_res$Exposure == "neuro", ]$Exposure = "Neuroticism"
 eczema_outcome_res[eczema_outcome_res$Exposure == "worry_cluster", ]$Exposure = "Worry"
 eczema_outcome_res[eczema_outcome_res$Exposure == "depressed_cluster", ]$Exposure = "Depressed affect"
 eczema_outcome_res[eczema_outcome_res$Exposure == "SESA", ]$Exposure = "SESA"
 eczema_outcome_res$Exposure = factor(eczema_outcome_res$Exposure, levels = c("Neuroticism", "Depressed affect", "SESA", "Worry"))
-library(ggplot2)
-library(dplyr)
 eczema_outc_plot = eczema_outcome_res$Method <- factor(eczema_outcome_res$Method, 
-                                                             levels = c("MR-PRESSO", "Weighted median", "MR Egger-SIMEX", "IVW"))
+                                                             levels = c("MR-PRESSO", "Weighted median", "MR-Egger/SIMEX", "IVW"))
 eczema_outcome_plot = ggplot(eczema_outcome_res,
                                 aes(x = OR,
                                     y = reorder(Exposure, desc(Exposure)),
@@ -338,10 +344,10 @@ eczema_outcome_plot = ggplot(eczema_outcome_res,
              linetype = "dotted",
              color = "black") +
   scale_color_manual(values = c("IVW" = "#990438",
-                                "MR Egger-SIMEX" = "#FFC107",
+                                "MR-Egger/SIMEX" = "#FFC107",
                                 "Weighted median" = "#004D40",
                                 "MR-PRESSO" = "#1E88E5"),
-                     breaks = c("IVW", "Weighted median", "MR Egger-SIMEX", "MR-PRESSO")) +
+                     breaks = c("IVW", "Weighted median", "MR-Egger/SIMEX", "MR-PRESSO")) +
   theme_classic(base_size = 16) +
   labs(x = "Odds Ratio (95% Confidence Interval)",
        y = "",
@@ -354,3 +360,9 @@ eczema_outcome_plot = ggplot(eczema_outcome_res,
     strip.background = element_rect(colour = "white", fill = "white")
   ) +
   coord_cartesian(xlim = c(0.5, 3))
+pdf("D:/data/skin_neuroticism/clusters/Fig2.pdf",
+width = 12,
+height = 5
+)
+cowplot::plot_grid(eczema_exposure_plot, eczema_outcome_plot, rel_widths = c(1, 1.2), scale = 0.9, labels = "auto")
+dev.off()
